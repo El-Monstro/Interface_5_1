e6fc2c683c3e856531735e084cb3ff42d3999a37
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-11-10 16:57:07 +0500

    Добавьте файлы проекта.

---
+++ Interface/ToursPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interface
+{
+    /// <summary>
+    /// Логика взаимодействия для ToursPage.xaml
+    /// </summary>
+    public partial class ToursPage : Page
+    {
+        public ToursPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void SearchTour_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new SearchTourPage());
+        }
+
+        private void MyOrders_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new MyOrdersPage());
+        }
+    }
+}
